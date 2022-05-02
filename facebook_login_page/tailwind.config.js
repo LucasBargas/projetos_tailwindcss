dbf655065b10de899784ae792dd47a1c392c22f5
lucas.bargasoficial@gmail.com <lucas.bargasoficial@gmail.com>
2022-05-02 10:16:29 -0300

    Imagens adicionadas e cores configuradas

--- facebook_login_page/tailwind.config.js
+++ facebook_login_page/tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '980': '980px'
+      },
+      colors: {
+        'fb-blue': '#f0f2f5',
+        'fb-btn-blue': '#1877f2',
+        'fb-btn-green': '#42b72a'
+      }
+    },
   },
   plugins: [],
 }
