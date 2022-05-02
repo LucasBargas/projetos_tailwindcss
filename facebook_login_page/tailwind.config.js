175585ea7128f18510ecae44342c4e4d314250a8
lucas.bargasoficial@gmail.com <lucas.bargasoficial@gmail.com>
2022-05-02 15:14:38 -0300

    Footer e Modal - desenvolvidos

--- facebook_login_page/tailwind.config.js
+++ facebook_login_page/tailwind.config.js
@@ -25,7 +25,8 @@ module.exports = {
         'fb-card': '#f5f6f7',
         'fb-red': '#fa3e3e',
         'fb-border': '#dddfe2',
-        'black-opacity': 'rgba(0, 0, 0, .3)'
+        'black-opacity': 'rgba(0, 0, 0, .3)',
+        'modal': 'rgba(0, 0, 0, .9)'
       },
 
       boxShadow: {
@@ -33,6 +34,23 @@ module.exports = {
         'notification': '-1px 1px 2px 0 rgba(0, 0, 0, .3)',
         'form': '0 2px 4px rgb(0, 0, 0, .1), 0 8px 16px rgb(0, 0, 0, .1)',
         'focus': '0 0 0 2px #e7f3ff'
+      },
+
+      keyframes: {
+        modalAnimate: {
+          from: {
+            opacity: '0',
+            transform: 'translateY(-50px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)',
+          }
+        }
+      },
+
+      animation: {
+        'show-modal': 'modalAnimate .6s forwards'
       },
 
       screens: {
