0cf3cd6f9dc941fc1b03404684dc07c9d43e702e
lucas.bargasoficial@gmail.com <lucas.bargasoficial@gmail.com>
2022-05-03 15:05:55 -0300

    Páginas adicionadas a rota

--- me_family/src/App.js
+++ me_family/src/App.js
@@ -1,14 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './layout/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
+import OurStory from './pages/OurStory/OurStory';
+import Events from './pages/Events/Events';
+import Gallery from './pages/Gallery/Gallery';
+import Contact from './pages/Contact/Contact';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='our-story' element={<OurStory />} />
+          <Route path='events' element={<Events />} />
+          <Route path='gallery' element={<Gallery />} />
+          <Route path='contact' element={<Contact />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   )
 }
