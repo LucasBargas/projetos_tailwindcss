f2f23861b031fb0fbccbbac9ba0a706729e4dfb2
lucas.bargasoficial@gmail.com <lucas.bargasoficial@gmail.com>
2022-05-04 00:45:54 -0300

    Identificação da troca de rotas com classe Active - aplicada

--- me_family/src/layout/Header/NavHeader/NavHeader.js
+++ me_family/src/layout/Header/NavHeader/NavHeader.js
@@ -1,33 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { IoClose } from 'react-icons/io5';
 import { NavLink } from 'react-router-dom';
 
 const NavHeader = () => {
   const [menuMobile, setMenuMobile] = useState(null);
+  const navLinks = useRef();
 
   const handleOutsideClick = ({ target, currentTarget }) => {
     if (target === currentTarget) setMenuMobile(null);
   }
 
+  const handleClick = ({ target }) => {
+    if (menuMobile) setMenuMobile(null);
+    if (target.classList.contains('active')) target.classList.add('text-sky-blue');
+
+    setTimeout(() => {
+      if (target.classList.contains('active')) target.classList.add('text-sky-blue');
+    }, 50);
+  }
+
+  useEffect(() => {
+    const navLinksArr = Array.from(navLinks.current.children);
+    navLinksArr.forEach(link => {
+      if (link.children[0].classList.contains('active')) link.children[0].classList.add('text-sky-blue');
+    })
+  }, []);
+
   return (
     <div>
       <nav onClick={handleOutsideClick} className={`${menuMobile ? 'flex' : 'hidden'} flex-col-reverse items-end fixed top-0 left-0 z-50 h-screen w-full p-4 bg-menu-mobile 276:p-3 lg:flex lg:flex-row lg:static lg:h-auto lg:w-auto lg:bg-white lg:p-0`}>
-        <ul className='flex flex-col px-5 py-2 bg-white h-screen w-full overflow-y-auto rounded-sm lg:h-auto lg:flex-row lg:p-0 lg:rounded-none lg:w-auto'>
+        <ul ref={navLinks} className='flex flex-col px-5 py-2 bg-white h-screen w-full overflow-y-auto rounded-sm lg:h-auto lg:flex-row lg:p-0 lg:rounded-none lg:w-auto'>
           <li>
-            <NavLink onClick={() => setMenuMobile(null)} className='block font-medium text-[15px] py-3 lg:pl-8 lg:py-2 text-dark-blue' end to="/">Home</NavLink>
+            <NavLink onClick={handleClick} className='block font-medium text-[15px] py-3 lg:pl-8 lg:py-2 text-dark-blue' end to="/">Home</NavLink>
           </li>
           <li>
-            <NavLink onClick={() => setMenuMobile(null)} className='block font-medium text-[15px] py-3 lg:pl-8 lg:py-2 text-dark-blue' to="/our-story">Our Story</NavLink>
+            <NavLink onClick={handleClick} className='block font-medium text-[15px] py-3 lg:pl-8 lg:py-2 text-dark-blue' to="/our-story">Our Story</NavLink>
           </li>
           <li>
-            <NavLink onClick={() => setMenuMobile(null)} className='block font-medium text-[15px] py-3 lg:pl-8 lg:py-2 text-dark-blue' to="/events">Events</NavLink>
+            <NavLink onClick={handleClick} className='block font-medium text-[15px] py-3 lg:pl-8 lg:py-2 text-dark-blue' to="/events">Events</NavLink>
           </li>
           <li>
-            <NavLink onClick={() => setMenuMobile(null)} className='block font-medium text-[15px] py-3 lg:pl-8 lg:py-2 text-dark-blue' to="/gallery">Gallery</NavLink>
+            <NavLink onClick={handleClick} className='block font-medium text-[15px] py-3 lg:pl-8 lg:py-2 text-dark-blue' to="/gallery">Gallery</NavLink>
           </li>
           <li>
-            <NavLink onClick={() => setMenuMobile(null)} className='block font-medium text-[15px] py-3 lg:pl-8 lg:py-2 text-dark-blue' to="/contact">Contact</NavLink>
+            <NavLink onClick={handleClick} className='block font-medium text-[15px] py-3 lg:pl-8 lg:py-2 text-dark-blue' to="/contact">Contact</NavLink>
           </li>
         </ul>
 
