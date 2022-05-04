45747c6054e2155a47bcf7be1a81cf2e217220b7
lucas.bargasoficial@gmail.com <lucas.bargasoficial@gmail.com>
2022-05-04 00:17:19 -0300

    Alturas da seção de Slider - em definição

--- me_family/src/pages/HomePage/HomePage.js
+++ me_family/src/pages/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div className='bg-red-500 h-[calc(100vh-72px)] 390:h-[calc(100vh-58px)] lg:h-auto'>
+    <div className='bg-red-500 h-[calc(100vh-72px)] lg:h-[calc(80vh-72px)] xl:min-h-[500px] 2xl:h-[calc(80vh-72px)] 390:h-[calc(100vh-58px)]'>
       homepage
     </div>
   )
